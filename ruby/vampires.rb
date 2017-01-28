1cdec0d04c23e2488e5f58cd7cbe84d03d3a27a4
joshmun <joshuamun@gmail.com>
2017-01-28 13:20:13 -0800

    Add employee processing loop

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,66 +1,74 @@
 #Driver Code
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will you be processing?"
+employees = gets.chomp.to_i
+current_employee = 1
 
-puts "How old will you be this year?"
-age = gets.chomp.to_i
+while current_employee <= employees
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "What year were you born?"
-born = gets.chomp.to_i
+	puts "How old will you be this year?"
+	age = gets.chomp.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic_bread = gets.chomp
+	puts "What year were you born?"
+	born = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance?"
-health = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	garlic_bread = gets.chomp
 
-#Boolean Conidition Declarations
-#Year comparison against age
-year = 2017
-born_true = (year - born) == age
-#if born_true == born
-#	p "You are a vampire!"
-#end
+	puts "Would you like to enroll in the company's health insurance?"
+	health = gets.chomp
 
-#Setting variables
-yes = "yes", "Yes"
-no = "no", "No"
+	#Boolean Conidition Declarations
+	#Year comparison against age
+	year = 2017
+	born_true = (year - born) == age
+	#if born_true == born
+	#	p "You are a vampire!"
+	#end
 
-#Garlic Bread
-gb_true = garlic_bread == yes
+	#Setting variables
+	yes = "yes", "Yes"
+	no = "no", "No"
 
-#Insurance
-health_true = health == yes
+	#Garlic Bread
+	gb_true = garlic_bread == yes
 
-#If the employee got their age right, 
-#and is willing to eat garlic bread 
-#or sign up for insurance, 
-#the result is “Probably not a vampire.”
-case
-	when born_true && (gb_true || health_true)
-		p "Probably not a vampire."
+	#Insurance
+	health_true = health == yes
 
-#If the employee got their age wrong, 
-#and hates garlic bread or waives insurance, 
-#the result is “Probably a vampire.”
+	#If the employee got their age right, 
+	#and is willing to eat garlic bread 
+	#or sign up for insurance, 
+	#the result is “Probably not a vampire.”
 
-	when !born_true && (!gb_true || !health_true)
-		p "Probably a vampire."
+		case
+			when born_true && (gb_true || health_true)
+				p "Probably not a vampire."
 
-#If the employee got their age wrong, 
-#hates garlic bread, and doesn’t want insurance, 
-#the result is “Almost certainly a vampire.”
+		#If the employee got their age wrong, 
+		#and hates garlic bread or waives insurance, 
+		#the result is “Probably a vampire.”
 
-	when !born_true && !gb_true && !health_true
-		p "Almost certainly a vampire."
+			when !born_true && (!gb_true || !health_true)
+				p "Probably a vampire."
 
-#Even if the employee is an amazing liar otherwise, 
-#anyone going by the name of “Drake Cula” or “Tu Fang” 
-#is clearly a vampire, because come on. 
-#In that case, you should print “Definitely a vampire.”
+		#If the employee got their age wrong, 
+		#hates garlic bread, and doesn’t want insurance, 
+		#the result is “Almost certainly a vampire.”
 
-	when name == "Drake Cula" || name == "Tu Fang"
-		p "Definitely a vampire."
+			when !born_true && !gb_true && !health_true
+				p "Almost certainly a vampire."
 
-	else puts "Results inconclusive."
+		#Even if the employee is an amazing liar otherwise, 
+		#anyone going by the name of “Drake Cula” or “Tu Fang” 
+		#is clearly a vampire, because come on. 
+		#In that case, you should print “Definitely a vampire.”
+
+			when name == "Drake Cula" || name == "Tu Fang"
+				p "Definitely a vampire."
+
+			else puts "Results inconclusive."
+		end
+	current_employee += 1
 end
