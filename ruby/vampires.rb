0ce7a5c7145998e18818818502312886a6bb2fcd
joshmun <joshuamun@gmail.com>
2017-01-28 11:31:15 -0800

    Initial commit, add driver code

---
+++ ruby/vampires.rb
@@ -0,0 +1,13 @@
+#Driver Code
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+
+puts = "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts = "Would you like to enroll in the company's health insurance?"
+health = gets.chomp
+
