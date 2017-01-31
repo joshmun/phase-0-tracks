205a6f6e9769f4a8c9fb42bee98efc56d03a56b7
joshmun <joshuamun@gmail.com>
2017-01-31 15:11:22 -0800

    Add additional methods against arrays and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -27,3 +27,38 @@ puts hollywood_stars
 harry_potter.each do |actor, character|
 	puts "#{actor} plays the character #{character} in the Harry Potter movie series."
 end
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+number_hash = {
+	0 => "Zero",
+	1 => "One",
+	2 => "Two",
+	3 => "Three",
+	4 => "Four",
+	5 => "Five",
+	6 => "Six",
+	7 => "Seven",
+	8 => "Eight",
+	9 => "Nine",
+	10 => "Ten"
+}
+
+#Array Methods
+p numbers.delete_if { |x| x < 5}
+p numbers.select { |x| x < 5}
+p numbers.drop_while { |x| x < 5}
+p numbers.delete_if { |x| x >= 0}
+
+#Hash Methods
+p number_hash.delete_if {|key, value| key < 5}
+p number_hash.select {|key, value| key > 5} 
+p number_hash.keep_if { |key, value| key < 5}
+p number_hash.has_value?("Zero")
+p number_hash.has_value?("Twelve")
+
+
+
+
+
+
+
