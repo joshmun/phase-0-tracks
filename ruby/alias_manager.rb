3e9ad4c2ec4217fe84b318ac67fb75f44bc34ffd
joshmun <joshuamun@gmail.com>
2017-01-31 21:27:14 -0800

    Initial commit, add pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+#Pseudocode
+#Fake-Name Generator
+
+#define a method with a string as a single parameter
+#take the input and split it into an array
+#take that input and swap or reverse the order
+#combine them into a single array
+#then take all of the vowels and shift them to next vowel
+# => and take all of the consonants and shift them to next consonant
+#print that baby
