a2dc61550488fbad05e2ec48f8a9cce2220ee8d1
joshmun <joshuamun@gmail.com>
2017-01-31 23:00:38 -0800

    Add method to reverse order of array

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,3 +8,33 @@
 #then take all of the vowels and shift them to next vowel
 # => and take all of the consonants and shift them to next consonant
 #print that baby
+
+
+def swap(name)
+	vowels = ["a", "e", "i", "o", "u"]
+	name.split(" ").reverse.join(" ")	#reverses order of array
+	name.map { |vowels| }  
+
+
+		#rotates to next vowel
+	#ind_name = name.split(" ") #.reverse.join(" ")
+	#ind_name.reverse!
+	#together = ind_name.join(" ")
+	#vowels = together.select("a, e, i, o, u")
+	#nvowels = vowels.next
+	end
+
+
+p swap("Felicia Torres")
+
+=begin def v_forward(vowel)
+	vowels = ["a", "e", "i", "o", "u"]
+	vowel = vowel.split('')
+	v_new = vowel.map do |char|
+		if vowe
+
+	end
+
+
+p v_forward ("Torres Felicia")
+=end
