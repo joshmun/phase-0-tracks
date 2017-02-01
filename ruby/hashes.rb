924ec4b37a8c5f5fc45aac12adc352b75ed47a2f
joshmun <joshuamun@gmail.com>
2017-01-31 17:30:18 -0800

    Initial commit and add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,17 @@
+#Pseudocode
+#The program will ask the user these questions:
+#client name
+#age
+# => convert to integoer
+#number of children
+# => convert to integoer
+#decor theme
+#Do you live on the west coast?
+#Do you live on the east coast?
+#print the results and store in a hash
+#ask the user if all the information is correct
+#if not, provide same questions but as updating previous hash
+# => no loop, but utilize new gets.chomp to update symbols
+#if so, then skip by stating 'none'
+#print the results and exit
+
