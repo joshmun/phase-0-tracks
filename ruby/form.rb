34cdf6c641c13238b48527b2a15cf32982f36677
joshmun <joshuamun@gmail.com>
2017-01-31 17:24:04 -0800

    Add practice form

---
+++ ruby/form.rb
@@ -0,0 +1,27 @@
+post = {
+	date: "1/31/17",
+	title: "A new world",
+	author: "Josh Mun",
+	body: "This is a test blog post"
+}
+
+p post
+
+# Driver Code
+puts "What is your title?"
+title = gets.chomp
+post[:title] = title
+
+puts "What is your name?"
+name = gets.chomp
+post[:author] = name
+
+puts "What do you want to say??"
+blog = gets.chomp
+post[:body] = blog
+
+p post
+
+post[:author] + post[:title]
+
+p post
