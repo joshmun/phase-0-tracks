d9cec76a9f55bec78d8dcac0fb2f2f98c45f3453
joshmun <joshuamun@gmail.com>
2017-01-31 18:25:48 -0800

    Add print lines for accessing nested data structures

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -2,27 +2,32 @@ eg = {
 	axe: [
 		'Telecaster',
 		'Stratocaster',
-	]
-	:pedals {
+	],
+	pedals: {
 		overdrive: [
 			'Rat',
 			'Euphoria',
 			'TS9',
 			'Klon Klone'
-		]
+		],
 		delay: [
-			'DD-7'
-			'Nova Delay'
-			'DD-20'
+			'DD-7',
+			'Nova Delay',
+			'DD-20',
 			'Bellwether'
 		]
-	}
-	:power [
+	},
+	power: [
 		'One Spot',
 		'DC 10 CIOKS'
 	]
 }
 
+p eg
+
+puts "After accessing nested data structures:"
 p eg[:axe][0]
-eg{:pedals}[delay].push('Carbon Copy')
-p eg{:pedals}[delay]
+eg[:pedals][:delay].push('Carbon Copy')
+p eg[:pedals][:delay]
+p eg[:power].reverse
+p eg[:pedals][:overdrive][3].swapcase
