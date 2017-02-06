86799300e354c4462bd33438573304c651fc480c
joshmun <joshuamun@gmail.com>
2017-02-05 23:15:36 -0800

    Add printouts of calculations and history

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -9,23 +9,58 @@ def calculator(num1, operator, num2)
 	when operator == '/'
 		answer = num1 / num2
 	end
-	puts answer
+	p answer
 end
 
-#Driver Code
-quit = ""
-until quit == 'quit'
-puts "What is the first number in your calculation?"
-num1 = gets.chomp.to_i
-
-puts "What is your operator?"
-operator = gets.chomp
-
-puts "What is your second number?"
-num2 = gets.chomp.to_i
-
-calculator(num1, operator, num2)
-
-puts "Please enter 'quit' to end, or enter to begin again."
-quit = gets.chomp
+# User Interface
+calc = ""
+calc_count = 0
+ops = {
+	compute: [],
+	answer: []
+}
+until calc == 'done'
+	puts "Please enter your calculation. Enter 'done' to finish."
+	calc = gets.chomp
+	ops[:compute].push(calc)
+		elements = calc.split("")
+		num1 = elements[0].to_i
+		operator = elements[1]
+		num2 = elements[2].to_i
+	answer = calculator(num1, operator, num2)
+	ops[:answer].push(answer)
+	calc_count += 1
+		if calc == 'done'
+		puts "#{calc_count-1} calculations performed:"
+			pcycle = 0
+				until pcycle == calc_count do
+				puts "#{ops[:compute][pcycle]} = #{ops[:answer][pcycle]}"
+				pcycle += 1
+			end
+		break
+		end
+	if answer == nil
+	puts "Sorry, What is the first number in your calculation?"
+	num1 = gets.chomp.to_i
+	
+	puts "What is your operator?"
+	operator = gets.chomp
+	
+	puts "What is your second number?"
+	num2 = gets.chomp.to_i
+	
+	broken = "#{num1}#{operator}#{num2}"
+	ops[:compute].push(broken)
+	
+	new_answer = calculator(num1, operator, num2)
+	ops[:answer].push(new_answer)
+	end
 end
+
+
+
+# #Driver Code
+# p calculator (3, '+', 2)
+# p calculator (100, '-', 10)
+# p calculator (23, '*', 3)
+# p calculator (100, '/', 10)
