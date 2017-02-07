fdec70bbbc860394244c67f373f5ddfe06ba204b
joshmun <joshuamun@gmail.com>
2017-02-06 22:53:46 -0800

    Initial commit, design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# Species ------
+# German Shepherd
+
+# Chracteristics -------
+# Friendly: yes
+# Loyal: yes
+# Name: varies
+# Color: varies
+# Legs: 4
+
+# Behavior ----
+# Jump
+# Sit
+# Fetch
