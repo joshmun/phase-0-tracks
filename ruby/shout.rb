8747d81fd52c5c6251d38c2545a3019d2c9d029d
joshmun <joshuamun@gmail.com>
2017-02-10 14:49:32 -0800

    Add Flight module, included in bird and plane class

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,34 @@
-module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
 
-	def self.yelling_happily (words)
-		words + "!!!" + " :)"
-	end
-end
+# 	def self.yelling_happily (words)
+# 		words + "!!!" + " :)"
+# 	end
+# end
 
-# Driver Code
-p Shout.yell_angrily("NO")
-p Shout.yelling_happily("Yay")
+# # Driver Code
+# p Shout.yell_angrily("NO")
+# p Shout.yelling_happily("Yay")
+
+# module Flight
+# 	def take_off(altitude)
+# 		puts "Taking off and ascending until reaching #{altitude}"
+# 	end
+# end
+
+# class Bird
+# 	include Flight
+# end
+
+# class Plane
+# 	include Flight
+# end
+
+# #Driver Code
+# bird = Bird.new
+# puts bird.take_off(800)
+
+# plane = Plane.new
+# puts plane.take_off(30000)
