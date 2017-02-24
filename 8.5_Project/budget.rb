7a5abd27fa83e6b9b163e90dd80c3d823de1d432
joshmun <joshuamun@gmail.com>
2017-02-24 13:53:28 -0800

    Add creating new user methods thatlimit username to 1 in database

--- 8.5_Project/budget.rb
+++ 8.5_Project/budget.rb
@@ -1,58 +1,81 @@
 # Business Logic #
 
-## Requiring Gems
+## Requiring Gems ##
 require 'sqlite3'
 
-## Setting out our database
+## Initializing database ##
 bdb = SQLite3::Database.new("budget.db")
 bdb.results_as_hash = true
 
-## SQL Commands
-create_username_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS username_table(
+## SQL Commands ##
+create_usernames_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS usernames(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255)
   )
 SQL
 
 create_budget_table = <<-SQL
-	CREATE TABLE IF NOT EXISTS budget_table(
+	CREATE TABLE IF NOT EXISTS budget(
 	id INTEGER PRIMARY KEY,
 	month INT,
 	caldate VARCHAR(255),
 	transaction INT, 
 	user_id INT,
-	FOREIGN KEY (user_id) REFERENCES username_table(id)
+	FOREIGN KEY (user_id) REFERENCES usernames(id)
 	)
 SQL
 
+bdb.execute(create_usernames_table)
+bdb.execute(create_budget_table)
+
+
 # Methods #
-## Create Method
-def create(bdb, username, create_username_table)
-	bdb.execute(create_username_table)
+
+## Creating New User Methods ##
+def add_user(bdb, username)
+	return "user already exists" if user_exists?(bdb, username)
 	bdb.execute(<<-SQL
-		INSERT INTO username_table (name) VALUES (username)
-	# 	CREATE UNIQUE INDEX IF NOT EXISTS username ON username_table(name)
+			INSERT INTO usernames (name) VALUES ("#{username}")
 	SQL
 	)
-	# puts "I've created a unique index"
-	# bdb.execute(<<-SQL
-	# 	INSERT INTO username_table(name) VALUES (username)
-	# SQL
-	# )
-	# puts "I've inserted into the username table"
-#   INSERT INTO username (name) 
-#   SELECT * FROM (SELECT username) AS tmp
-#   WHERE NOT EXISTS (
-#   	SELECT name FROM username WHERE name = username
-#   	) LIMIT 1;
-# SQL
-# )
 end
 
-## Login Method
+def users(bdb)
+	bdb.execute(<<-SQL
+		SELECT * FROM usernames
+	SQL
+	)
+end
+
+def user_exists?(bdb, username)
+	users(bdb).each do |user|
+		return true if user["name"] == username
+	end
+	false
+end
+
+
+## Login Methods ##
+
+
+
 def returning(bdb, username)
 end
+
+
+## Select users from usernames
+
+
+## Driver Code
+
+add_user(bdb, "josh_mun")
+p users(bdb)
+
+
+
+# users = users(bdb)
+
 
 # create_budget_table = <<-SQL
 # 	id INTEGER PRIMARY KEY,
@@ -76,28 +99,28 @@ end
 # Login User Interface
 ## Asks user to login or create a username. If creating, executes create method, if logging in, executes returning method
 
-puts "Hello! Are you a returning user or would you like to create a username?"
-login = gets.chomp
-confirmation = nil
+# puts "Hello! Are you a returning user or would you like to create a username?"
+# login = gets.chomp
+# confirmation = nil
 
-while (login != "create" || login != "returning")
-	if login == "create"
-		until confirmation == "yes"
-		puts "Please enter a username"
-		username = gets.chomp
-		puts "Please confirm 'yes' if #{username} is correct."
-		confirmation = gets.chomp
-			if confirmation == "yes"
-				create(bdb, username, create_username_table)
-			end
-		end
-	break	
-	elsif login == "returning"
-		puts "Welcome! Please enter your username."
-		username = gets.chomp
-		returning(bdb, username)
-		break
-	else puts "I'm sorry, I didn't understand that. Are you returning or would you like to create a username?"
-		login = gets.chomp
-	end
-end
+# while (login != "create" || login != "returning")
+# 	if login == "create"
+# 		until confirmation == "yes"
+# 		puts "Please enter a username"
+# 		username = gets.chomp
+# 		puts "Please confirm 'yes' if #{username} is correct."
+# 		confirmation = gets.chomp
+# 			if confirmation == "yes"
+# 				create(bdb, username, create_username_table)
+# 			end
+# 		end
+# 	break	
+# 	elsif login == "returning"
+# 		puts "Welcome! Please enter your username."
+# 		username = gets.chomp
+# 		returning(bdb, username)
+# 		break
+# 	else puts "I'm sorry, I didn't understand that. Are you returning or would you like to create a username?"
+# 		login = gets.chomp
+# 	end
+# end
