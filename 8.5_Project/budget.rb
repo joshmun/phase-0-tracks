6a2fcb222dcc0f6a1ac704c015b4e27e1333623d
joshmun <joshuamun@gmail.com>
2017-02-24 15:02:17 -0800

    Add method that receives initial transaction data, and translates month data method

--- 8.5_Project/budget.rb
+++ 8.5_Project/budget.rb
@@ -15,24 +15,23 @@ create_usernames_table = <<-SQL
   )
 SQL
 
-# create_budget_table = <<-SQL
-# 	CREATE TABLE IF NOT EXISTS budget(
-# 	id INTEGER PRIMARY KEY,
-# 	month INT,
-# 	caldate VARCHAR(255),
-# 	transaction INT, 
-# 	user_id INT,
-# 	FOREIGN KEY (user_id) REFERENCES usernames(id)
-# 	)
-# SQL
+create_budget_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS budgets(
+	id INTEGER PRIMARY KEY,
+	month VARCHAR(255),
+	caldate VARCHAR(255),
+	details VARCHAR(255),
+	cost REAL, 
+	user_id INT,
+	FOREIGN KEY (user_id) REFERENCES usernames(id)
+	)
+SQL
 
 bdb.execute(create_usernames_table)
-# bdb.execute(create_budget_table)
+bdb.execute(create_budget_table)
 
-## Declared Variables ##
-
-# Methods #
-
+## Methods ##
+#----------------------LOGIN METHODS-------------------------#
 ## Creating New User Methods ##
 def add_user(bdb, username)
 	return "user already exists" if user_exists?(bdb, username)
@@ -57,7 +56,7 @@ def users(bdb)
 	)
 end
 
-## Login Methods ##
+## Returning User Login Method ##
 def user_id(bdb, username)
 	user_id_num = nil
 	users(bdb).each do |user|
@@ -69,16 +68,29 @@ def user_id(bdb, username)
 	user_id_num
 end
 
-## Select users from usernames
+#-------------ENTERING NEW TRANSACTION METHODS--------------#
+def transaction(caldate, details, cost)
+	transaction_data = []
+	[caldate, details, cost].each { |element| transaction_data << element }
+	# transaction << caldate
+	# transaction << details
+	# transaction << cost
+	transaction_data
+end
 
-
+def month_translater(transaction_data)
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	caldate = transaction_data[0].split("/")
+	month = caldate[0].to_i
+	translated_month = months[month-1]
+end
 ## Driver Code
 
 add_user(bdb, "josh_mun")
 p users(bdb)
-p user_id(bdb, "josh_mun")
-
-
+p user_id_num = user_id(bdb, "josh_mun")
+transaction_data = transaction("2/24/17", "Costco hot dogs", 5.69)
+p month_translater(transaction_data)
 
 # users = users(bdb)
 
