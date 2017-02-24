9e03a1c5759fb16e7b0cec56a9a52cc53dfbb912
joshmun <joshuamun@gmail.com>
2017-02-24 12:53:30 -0800

    Edit create method, adds username into table, but need to fix insert on condition that unique

--- 8.5_Project/budget.rb
+++ 8.5_Project/budget.rb
@@ -1,50 +1,101 @@
+# Business Logic #
+
+## Requiring Gems
 require 'sqlite3'
 
-db = SQLite3::Database.new("budget.db")
-db.results_as_hash = true
+## Setting out our database
+bdb = SQLite3::Database.new("budget.db")
+bdb.results_as_hash = true
 
-# Create Method
-def create(username)
-	db.execute()
-end
-
-# Login Method
-def returning(username)
-end
+## SQL Commands
+create_username_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS username_table(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
 
 create_budget_table = <<-SQL
+	CREATE TABLE IF NOT EXISTS budget_table(
 	id INTEGER PRIMARY KEY,
 	month INT,
 	caldate VARCHAR(255),
 	transaction INT, 
 	user_id INT,
-	FOREIGN KEY (user_id) REFERENCES username(id)
+	FOREIGN KEY (user_id) REFERENCES username_table(id)
+	)
 SQL
 
+# Methods #
+## Create Method
+def create(bdb, username, create_username_table)
+	bdb.execute(create_username_table)
+	bdb.execute(<<-SQL
+		INSERT INTO username_table (name) VALUES (username)
+	# 	CREATE UNIQUE INDEX IF NOT EXISTS username ON username_table(name)
+	SQL
+	)
+	# puts "I've created a unique index"
+	# bdb.execute(<<-SQL
+	# 	INSERT INTO username_table(name) VALUES (username)
+	# SQL
+	# )
+	# puts "I've inserted into the username table"
+#   INSERT INTO username (name) 
+#   SELECT * FROM (SELECT username) AS tmp
+#   WHERE NOT EXISTS (
+#   	SELECT name FROM username WHERE name = username
+#   	) LIMIT 1;
+# SQL
+# )
+end
 
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
-  )
-SQL
+## Login Method
+def returning(bdb, username)
+end
+
+# create_budget_table = <<-SQL
+# 	id INTEGER PRIMARY KEY,
+# 	month INT,
+# 	caldate VARCHAR(255),
+# 	transaction INT, 
+# 	user_id INT,
+# 	FOREIGN KEY (user_id) REFERENCES username(id)
+# SQL
 
 
-# User Interface
+# create_table_cmd = <<-SQL
+#   CREATE TABLE IF NOT EXISTS kittens(
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(255),
+#     age INT
+#   )
+# SQL
+
+
+# Login User Interface
+## Asks user to login or create a username. If creating, executes create method, if logging in, executes returning method
+
 puts "Hello! Are you a returning user or would you like to create a username?"
 login = gets.chomp
+confirmation = nil
 
 while (login != "create" || login != "returning")
 	if login == "create"
+		until confirmation == "yes"
 		puts "Please enter a username"
 		username = gets.chomp
-		create(username)
-		break
+		puts "Please confirm 'yes' if #{username} is correct."
+		confirmation = gets.chomp
+			if confirmation == "yes"
+				create(bdb, username, create_username_table)
+			end
+		end
+	break	
 	elsif login == "returning"
 		puts "Welcome! Please enter your username."
 		username = gets.chomp
-		returning(username)
+		returning(bdb, username)
 		break
 	else puts "I'm sorry, I didn't understand that. Are you returning or would you like to create a username?"
 		login = gets.chomp
