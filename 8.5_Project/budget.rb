b2bf51a2e6e322c238d47cc730396eacc6ffbf62
joshmun <joshuamun@gmail.com>
2017-02-24 10:46:14 -0800

    Add user interfacee for welcoming login or returning user

--- 8.5_Project/budget.rb
+++ 8.5_Project/budget.rb
@@ -1,33 +1,34 @@
-require 'sqlite3'
+# require 'sqlite3'
 
-db = SQLite3::Database.new("budget.db")
-db.results_as_hash = true
+# db = SQLite3::Database.new("budget.db")
+# db.results_as_hash = true
 
-# Create Method
-def create(username)
-end
+# # Create Method
+# def create(username)
+# 	db.execute()
+# end
 
-# Login Method
-def login(username)
-end
+# # Login Method
+# def returning(username)
+# end
 
-create_budget_table = <<-SQL
-	id INTEGER PRIMARY KEY,
-	month INT,
-	caldate VARCHAR(255),
-	transaction INT, 
-	user_id INT,
-	FOREIGN KEY (user_id) REFERENCES username(id)
-SQL
+# create_budget_table = <<-SQL
+# 	id INTEGER PRIMARY KEY,
+# 	month INT,
+# 	caldate VARCHAR(255),
+# 	transaction INT, 
+# 	user_id INT,
+# 	FOREIGN KEY (user_id) REFERENCES username(id)
+# SQL
 
 
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    age INT
-  )
-SQL
+# create_table_cmd = <<-SQL
+#   CREATE TABLE IF NOT EXISTS kittens(
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(255),
+#     age INT
+#   )
+# SQL
 
 
 # User Interface
@@ -37,3 +38,10 @@ login = gets.chomp
 if login == "create"
 	puts "Please enter a username"
 	username = gets.chomp
+elsif login == "returning"
+	puts "Welcome! Please enter your username."
+	username = gets.chomp
+	returning(username)
+else puts "I'm sorry, I didn't understand that. Are you returning or would you like to create a username?"
+	login = gets.chomp
+end
