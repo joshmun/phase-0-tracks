b7aec50a2ad406ed0cd3b9d359630efc901fd422
joshmun <joshuamun@gmail.com>
2017-02-24 17:07:49 -0800

    Add user interface full, need to fix views calling all user data

--- 8.5_Project/budget.rb
+++ 8.5_Project/budget.rb
@@ -136,59 +136,119 @@ def average_month(bdb, month)
 	"You had #{num_of_transactions} transactions this #{month} with an average of $#{average} spent per day."
 end
 #----------------------USER INTERFACE-------------------------#
-# puts "Hello! Are you a returning user or would you like to create a username?"
-# login = gets.chomp
-# confirmation = nil
+puts "Hello! Are you a RETURNING user or would you like to CREATE a username?"
+login = gets.chomp
+confirmation = nil
 
-# while (login != "create" || login != "returning")
-# 	if login == "create"
-# 		until confirmation == "yes"
-# 		puts "Please enter a username"
-# 		username = gets.chomp
-# 		puts "Please confirm 'yes' if #{username} is correct."
-# 		confirmation = gets.chomp
-# 			if confirmation == "yes"
-# 				create(bdb, username, create_username_table)
-# 			end
-# 		end
-# 	break	
-# 	elsif login == "returning"
-# 		puts "Welcome! Please enter your username."
-# 		username = gets.chomp
-# 		returning(bdb, username)
-# 		break
-# 	else puts "I'm sorry, I didn't understand that. Are you returning or would you like to create a username?"
-# 		login = gets.chomp
-# 	end
-# end
+## LOGIN ##
+while (login != "create" || login != "returning")
+	if login == "create"
+		until confirmation == "yes"
+		puts "Please enter a username"
+		username = gets.chomp
+		puts "Please confirm 'yes' if #{username} is correct."
+		confirmation = gets.chomp
+			if confirmation == "yes"
+				add_user(bdb, username) if user_exists?(bdb, username) == false
+			end
+		end
+	break	
+	elsif login == "returning"
+		puts "Welcome! Please enter your username."
+		username = gets.chomp
+		user_id_num = user_id(bdb, username)
+		break
+	else puts "I'm sorry, I didn't understand that. Are you RETURNING or would you like to CREATE a username?"
+		login = gets.chomp
+	end
+end
 
+## OPTIONS ##
+
+log_off = ""
+while log_off != "log off"
+	puts "What would you like to do today? I can:"
+	puts "1. Enter a New Transaction"
+	puts "2. View All Transactions"
+	puts "3. View Transactions by Month"
+	puts "4. View Transactions by Caldate"
+	puts "5. Tell you your average spending per month"
+	
+	option = gets.chomp.to_i
+	case option
+		when option = 1
+			puts "Enter a transaction."
+			puts "Please enter the caldate. Example: 2/29/17"
+			caldate = gets.chomp
+			puts "Please enter details. Example: Costco"
+			details = gets.chomp
+			puts "Please enter the cost. Example: 19.72. Do not enter $."
+			cost = gets.chomp.to_i
+	
+			transaction_data = transaction(caldate, details, cost)
+			month_translater(transaction_data)
+			add_user_id(transaction_data, user_id_num)
+			store_transaction!(bdb, transaction_data)
+			puts "Entry successfully stored."
+	
+		when option = 2
+			puts "Here is a history of all of your transactions."
+			all_view = view_all_transactions(bdb, user_id_num)
+			tp all_view, "month", "caldate", "details", "cost", "name"
+	
+		when option = 3
+			puts "Please enter the month you'd like to view. Example: February 2017."
+			month = gets.chomp
+			month_view = view_transactions_month(bdb, user_id_num, month)
+			tp month_view, "month", "caldate", "details", "cost", "name"
+	
+		when option = 4
+			puts "Please enter the caldate you'd like to view. Example: 2/17/17."
+			caldate = gets.chomp
+			caldate_view = view_transactions_caldate(bdb, user_id_num, caldate)
+			tp caldate_view, "month", "caldate", "details", "cost", "name"
+		
+		when option = 5
+			puts "You selected: average spending."
+			puts "Please enter the month you'd like to view. Example: February 2017."
+			avg_month = gets.chomp
+			p average_month(bdb, avg_month)
+		else
+			puts "Would you like to log off, or view more options?"
+			log_off = gets.chomp
+	end
+	puts "Would you like to log off, or view more options?"
+	log_off = gets.chomp
+end
+
+puts "Thanks for using the Budgeting Project. Have a great day!"
 
 #----------------------Driver Code-------------------------#
 # Driver Code
 
 # add_user(bdb, "josh_mun")
-users(bdb)
-user_id_num = user_id(bdb, "josh_mun")
-# transaction_data = transaction("2/24/17", "Costco hot dogs", 5.69)
-# transaction_data2 = transaction("2/21/17", "Cash N Carry plates", 20)
-# transaction_data3 = transaction("2/19/17", "Dick's Burgers", 7.56)
-# month_translater(transaction_data)
-# month_translater(transaction_data2)
-# month_translater(transaction_data3)
-# add_user_id(transaction_data, user_id_num)
-# add_user_id(transaction_data2, user_id_num)
-# add_user_id(transaction_data3, user_id_num)
-# store_transaction!(bdb, transaction_data)
-# store_transaction!(bdb, transaction_data2)
-# store_transaction!(bdb, transaction_data3)
+# users(bdb)
+# user_id_num = user_id(bdb, "josh_mun")
+# # transaction_data = transaction("2/24/17", "Costco hot dogs", 5.69)
+# # transaction_data2 = transaction("2/21/17", "Cash N Carry plates", 20)
+# # transaction_data3 = transaction("2/19/17", "Dick's Burgers", 7.56)
+# # month_translater(transaction_data)
+# # month_translater(transaction_data2)
+# # month_translater(transaction_data3)
+# # add_user_id(transaction_data, user_id_num)
+# # add_user_id(transaction_data2, user_id_num)
+# # add_user_id(transaction_data3, user_id_num)
+# # store_transaction!(bdb, transaction_data)
+# # store_transaction!(bdb, transaction_data2)
+# # store_transaction!(bdb, transaction_data3)
 
-all_view = view_all_transactions(bdb, user_id_num)
-tp all_view, "month", "caldate", "details", "cost", "name"
+# all_view = view_all_transactions(bdb, user_id_num)
+# tp all_view, "month", "caldate", "details", "cost", "name"
 
-month_view = view_transactions_month(bdb, user_id_num, "February 2017")
-tp month_view, "month", "caldate", "details", "cost", "name"
+# month_view = view_transactions_month(bdb, user_id_num, "February 2017")
+# tp month_view, "month", "caldate", "details", "cost", "name"
 
-caldate_view = view_transactions_caldate(bdb, user_id_num, "2/21/17")
-tp caldate_view, "month", "caldate", "details", "cost", "name"
+# caldate_view = view_transactions_caldate(bdb, user_id_num, "2/21/17")
+# tp caldate_view, "month", "caldate", "details", "cost", "name"
 
-p average_month(bdb, "February 2017")
+# p average_month(bdb, "February 2017")
